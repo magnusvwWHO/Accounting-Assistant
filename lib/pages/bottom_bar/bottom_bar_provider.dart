8cbdb70bccfe5ca4246988398838c82c304735f2
thesapph1re <itzsapphireone@gmail.com>
2023-10-17 14:06:50 +0300

    Промежуточное обновление
    Изменение способа работы с контекстом в bottom_bar_provider

--- lib/pages/bottom_bar/bottom_bar_provider.dart
+++ lib/pages/bottom_bar/bottom_bar_provider.dart
@@ -4,14 +4,14 @@ import 'package:go_router/go_router.dart';
 class BottomBarProvider extends ChangeNotifier {
   BottomBarProvider({required this.context});
 
-  final BuildContext context;
+  BuildContext context;
   final pages = ['/list', '/task'];
 
   int currentIndex = 0;
 
   dynamic onTap(int index) {
     currentIndex = index;
-    GoRouter.of(context).go(pages[currentIndex]);
+    GoRouter.of(context).go(pages[index]);
     notifyListeners();
   }
 }
