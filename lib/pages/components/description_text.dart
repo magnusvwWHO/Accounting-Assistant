610e18cbdd6df77de8c2c9e919a0880db38b62d7
thesapph1re <itzsapphireone@gmail.com>
2023-10-30 12:39:49 +0300

    Классы для текстов

---
+++ lib/pages/components/description_text.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class DescriptionText extends StatelessWidget {
+  const DescriptionText({super.key, required this.text});
+
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      style: const TextStyle(
+        fontWeight: FontWeight.w600,
+        fontSize: 24.0,
+      ),
+    );
+  }
+}
